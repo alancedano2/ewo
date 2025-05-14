89fbff8a4f755ec6d8e26b623ad34e8455c1b48e
alancedano2 <supernatura94@yahoo.com>
2025-05-14 09:47:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const carousel = document.querySelector(".carousel-container");
+  const carousel = document.querySelector('.carousel-container');
+  const totalSlides = document.querySelectorAll('.carousel-item').length;
   let index = 0;
-  const totalSlides = document.querySelectorAll(".carousel-item").length;
 
-  // Cambia el carrusel cada 5 segundos
   setInterval(() => {
     index = (index + 1) % totalSlides;
     carousel.style.transform = `translateX(-${index * 100}%)`;
-  }, 5000);
+  }, 5000); // Cada 5 segundos cambia la imagen
 });
