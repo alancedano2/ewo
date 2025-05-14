e7a1d8a50fa714957eccdda8ee07f46673c08f89
alancedano2 <supernatura94@yahoo.com>
2025-05-14 09:42:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const carousel = document.querySelector(".carousel-slide");
+  const carousel = document.querySelector(".carousel-container");
   let index = 0;
   const totalSlides = document.querySelectorAll(".carousel-item").length;
 
-  // Cambia el slide cada 5 segundos
+  // Cambia el carrusel cada 5 segundos
   setInterval(() => {
     index = (index + 1) % totalSlides;
     carousel.style.transform = `translateX(-${index * 100}%)`;
