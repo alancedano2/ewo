285325d5d97ceb82392d9c002ab3461bf7d4624b
alancedano2 <supernatura94@yahoo.com>
2025-08-10 22:17:31 -0400

    Update page.tsx

--- src/app/eventos/page.tsx
+++ src/app/eventos/page.tsx
@@ -67,7 +67,7 @@ const EventosPage: React.FC = () => {
             {/* Cuadro de información a la derecha */}
             <div className="bg-gray-900 bg-opacity-70 rounded-lg shadow-lg p-6 md:w-1/3 h-fit">
               <h2 className="text-xl font-semibold mb-4 text-yellow-400">¿Quieres ver nuevos eventos?</h2>
-              <p className="text-gray-300 mb-2">Para ver los nuevos eventos, sintoniza <strong>'EWO El Update'</strong> todos los sábados a las 10 AM en la página de <Link href="/programas" className="text-blue-400 hover:underline"><strong>Programas</strong></Link>.</p>
+              <p className="text-gray-300 mb-2">Para ver los nuevos eventos, sintoniza <strong>&apos;EWO El Update&apos;</strong> todos los sábados a las 10 AM en la página de <Link href="/programas" className="text-blue-400 hover:underline"><strong>Programas</strong></Link>.</p>
               <p className="text-gray-300">Allí se anuncian los nuevos eventos. También puedes visitar el <Link href="https://www.facebook.com/EWO_PR" target="_blank" className="text-blue-400 hover:underline"><strong>Facebook de la EWO</strong></Link> para ver si hay un nuevo evento.</p>
             </div>
           </div>
